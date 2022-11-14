234fd71d0243a155255652d3bb71097659517c79
Augustodv <augustodevera@gmail.com>
2022-11-14 17:05:48 -0300

    fix workflows

--- karma.conf.js
+++ karma.conf.js
@@ -24,22 +24,6 @@ module.exports = function (config) {
     jasmineHtmlReporter: {
       suppressAll: true // removes the duplicated traces
     },
-    coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/<project-name>'),
-      subdir: '.',
-      reporters: [
-        { type: 'html' },
-        { type: 'text-summary' }
-      ],
-      check: {
-        global: {
-          statements: 80,
-          branches: 80,
-          functions: 80,
-          lines: 80
-        }
-      }
-    },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
